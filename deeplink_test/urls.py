7661eb7f69a7f3ea9fd31070ea2ac75fe2cda076
Robert Alvarez <alvarezrobert150@gmail.com>
2023-11-08 16:49:18 -0400

    ajust

--- deeplink_test/urls.py
+++ deeplink_test/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from landing.views import landing, well_known
+from landing.views import landing, well_known, pokemons
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('landing/', landing, name='landing'),
+    path('pokemons/', pokemons, name='pokemons'),
     path('.well-known/assetlinks.json', well_known, name='well-known')
 ]
--- landing/views.py
+++ landing/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 def landing(request):
     return render(request, 'landing.html', {})
 
+def pokemons(request):
+    return render(request, 'pokemons.html', {})
+
 
 def well_known(request):
     return render(request, 'assetlinks.json', {}, content_type='application/json')
