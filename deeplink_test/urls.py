25f71e3d7c5849b5abfeaf7c65a82da50bece0de
Robert Alvarez <alvarezrobert150@gmail.com>
2023-11-08 11:56:18 -0400

    added assetlink render

--- deeplink_test/urls.py
+++ deeplink_test/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from landing.views import landing
+from landing.views import landing, well_known
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('landing/', landing, name='landing'),
+    path('.well-known/assetlinks.json', well_known, name='well-known')
 ]
--- landing/views.py
+++ landing/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def landing(request):
     return render(request, 'landing.html', {})
+
+
+def well_known(request):
+    return render(request, 'assetlinks.json', {})
